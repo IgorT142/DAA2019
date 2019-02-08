911a70808c708adb6e8371e930cbad7af70794d6
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-02-08 17:27:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
-
-    public static void main (String[] args){
+    public static void main(String[] args){
+        int n,m,s;
         Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        for(int i=0;i<N;i++){
-            System.out.println("Hello World!");
+        String datos = scan.nextLine();
+        String[] datosSeparados = datos.split(" ");
+        /*Asignacion de datos del string a variables*/
+        n = Integer.parseInt(datosSeparados[0]);
+        m = Integer.parseInt(datosSeparados[1]);
+        s = Integer.parseInt(datosSeparados[2]);
+        String[] lista = new String[m];
+        String[][] listaVertices = new String[n][];
+        ArrayList[] lista3 = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            String vertices = scan.nextLine();
+            String[] verticesSeparados = vertices.split(" ");
+            int v = Integer.parseInt(verticesSeparados[0]);
+            int u = Integer.parseInt(verticesSeparados[1]);
+            listaVertices[i] = verticesSeparados;
+            lista3[v].add(u);
+            lista3[u].add(v);
         }
+        System.out.println(lista3[0]);
     }
 }
