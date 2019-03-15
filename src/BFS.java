85bc095b56019ef38ced45f00d78683c57ae0574
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-03-15 19:55:01 +0100

    Added last exercises

---
+++ src/BFS.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nNodos, nAristas, nodoInicial;
+        //System.out.println("Numero de nodos");
+        nNodos = scan.nextInt();
+        //System.out.println("Numero de aristas");
+        nAristas = scan.nextInt();
+       // System.out.println("Nodo inicial");
+        nodoInicial = scan.nextInt();
+
+        ArrayList<Integer> list[] = new ArrayList[nNodos+1];
+        for (int i = 0; i <= nNodos; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < nAristas; i++) {
+            int v = scan.nextInt();
+            int u = scan.nextInt();
+            list[v].add(u);
+            list[u].add(v);
+        }
+        bfs(list,nodoInicial,nNodos);
+    }
+
+    public static void bfs(ArrayList<Integer> lista[], int nInicial, int nNodos) {
+        List<Integer> traversal = new ArrayList<>(lista.length);
+        Queue<Integer> cola = new LinkedList<>();
+        boolean[] visitados = new boolean[lista.length];
+        visitados[nInicial] = true;
+        traversal.add(nInicial);
+        cola.add(nInicial);
+        while (!cola.isEmpty()) {
+            int aux = cola.remove();
+            for (int adj : lista[aux]) {
+                if (!visitados[adj]) {
+                    visitados[adj] = true;
+                    traversal.add(adj);
+                    cola.add(adj);
+                }
+            }
+        }
+        for(int i = 0;i<traversal.size();i++) {
+            System.out.print(traversal.get(i) + " ");
+        }
+    }
+}
