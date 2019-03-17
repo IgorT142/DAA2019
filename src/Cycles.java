ca8266c1e7377e8c659d173e8144f5bccbee50d8
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-03-17 15:19:51 +0100

    Cycles completed algorythm
    Judge fails at testing output 6, same output works in my IDE though.

--- src/Cycles.java
+++ src/Cycles.java
@@ -5,14 +5,14 @@ public class Cycles {
     public static void main(String[] args) {
         boolean ciclo = false;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Numero de nodos");
+        //System.out.println("Numero de nodos");
         int nNodos = scan.nextInt();
-        System.out.println("Numero de aristas");
+       // System.out.println("Numero de aristas");
         int nAristas = scan.nextInt();
-        System.out.println("Introduzca aristas");
+       // System.out.println("Introduzca aristas");
 
         List<ArrayList<Integer>> listaAdyacencias = new ArrayList<>(nNodos);
-        for (int i = 0; i < nNodos; i++) {
+        for (int i = 0; i < nNodos+1; i++) {
             listaAdyacencias.add(i, new ArrayList<>());
         }
         for (int i = 0; i < nAristas; i++) {
@@ -22,17 +22,17 @@ public class Cycles {
         }
 
         Queue<Integer> cola = new LinkedList<>();
-        boolean[] visitados = new boolean[nNodos];
+        boolean[] visitados = new boolean[nNodos+1];
             for (int i = 0; i < listaAdyacencias.size(); i++) {
                 visitados[i] = true;
                 if(!listaAdyacencias.get(i).isEmpty()) {
-                    for (int j = 0; j < listaAdyacencias.get(j).size(); j++) {
+                    for (int j = 0; j < listaAdyacencias.get(i).size(); j++) {
                         if(visitados[i] && visitados[listaAdyacencias.get(i).get(j)]){
-                            System.out.println("Ciclo");
+                            System.out.println("true");
                             System.exit(1);
                         }
                         if (visitados[listaAdyacencias.get(i).get(j)]) {
-                            System.out.println("Ciclo");
+                            System.out.println("true");
                             System.exit(1);
                         } else {
                             visitados[listaAdyacencias.get(i).get(j)] = true;
@@ -41,6 +41,6 @@ public class Cycles {
                     }
                 }
             }
-        System.out.println("No ciclo");
+        System.out.println("false");
     }
 }
