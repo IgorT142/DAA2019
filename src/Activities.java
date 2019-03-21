981edaa9c4eeae25f6af1498be0b104abe577485
igort142 <igort142@gmail.com>
2019-03-21 15:59:53 +0100

    Activitities

---
+++ src/Activities.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Activities {
+    public static void main(String[] args) {
+        int casos,actividades;
+        Scanner scan = new Scanner(System.in);
+        casos = scan.nextInt();
+
+    }
+}
