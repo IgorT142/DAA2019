cc51266451a797b0cde38ac86028fad57c5e42c7
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-04-25 01:45:35 +0200

    Sudoku (half done)

--- src/Segundo_Parcial/Labyrinth.java
+++ src/Segundo_Parcial/Labyrinth.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Labyrinth {
-private static int pasosMin;
+    private static int pasosMin;
+
     public static void main(String[] args) {
-        int sol= 655;
+        int sol = 655;
         Scanner scan = new Scanner(System.in);
         int N;
         N = scan.nextInt();
@@ -18,9 +19,9 @@ private static int pasosMin;
                 matriz[i].add(scan.nextInt());
             }
         }
-        pasosMin = N*N;
+        pasosMin = N * N;
         backTracking(matriz, 0, 0, N, 1);
-        if(pasosMin!=N*N) {
+        if (pasosMin != N * N) {
             System.out.println(pasosMin);
         } else {
             System.out.println("SIN SOLUCION");
@@ -28,29 +29,29 @@ private static int pasosMin;
     }
 
     public static void backTracking(List<Integer> matriz[], int x, int y, int N, int pasos) {
-       // System.out.println("Estoy en : " + x + " , " + y);
-        matriz[y].set(x,pasos);
+        // System.out.println("Estoy en : " + x + " , " + y);
+        matriz[y].set(x, pasos);
         if (x == N - 1 && y == N - 1) {
-            if(pasos<pasosMin){
-                pasosMin=pasos;
+            if (pasos < pasosMin) {
+                pasosMin = pasos;
             }
         }
         if (x < N - 1) {
             if (matriz[y].get(x + 1) == 0) {
-                backTracking(matriz, x + 1, y, N, pasos+1);
-                matriz[y].set(x+1,0);
+                backTracking(matriz, x + 1, y, N, pasos + 1);
+                matriz[y].set(x + 1, 0);
             }
         }
         if (y < N - 1) {
             if (matriz[y + 1].get(x) == 0) {
-                backTracking(matriz, x, y + 1, N, pasos+1);
-                matriz[y+1].set(x,0);
+                backTracking(matriz, x, y + 1, N, pasos + 1);
+                matriz[y + 1].set(x, 0);
             }
         }
         if (y >= 1) {
             if (matriz[y - 1].get(x) == 0) {
-                backTracking(matriz, x, y - 1, N, pasos+1);
-                matriz[y-1].set(x,0);
+                backTracking(matriz, x, y - 1, N, pasos + 1);
+                matriz[y - 1].set(x, 0);
             }
         }
         if (x >= 1) {
